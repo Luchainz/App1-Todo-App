e252158900b33339e7798403dc29eadeba4f46c3
Luchainz <djluchainz@gmail.com>
2023-07-05 15:32:22 -0400

    Change theme, add clock, handle edit and complete erorr. Changed "main" to "cli"

--- web.py
+++ web.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import functions
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
 todos = functions.get_todos()
 
 st.title("My Todo App")
@@ -11,4 +16,5 @@ st.write("This app is to increase your productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label="Add a new todo", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
