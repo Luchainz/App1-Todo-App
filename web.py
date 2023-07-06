fdd1e5621a208848963bdbf6316850b80b0ad115
Luchainz <djluchainz@gmail.com>
2023-07-06 13:21:51 -0400

    debugging

--- web.py
+++ web.py
@@ -21,5 +21,5 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun() #needed for checkboxes
 
-st.text_input(label="", placeholder="Add new todo...",
+st.text_input(label="'todos", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
